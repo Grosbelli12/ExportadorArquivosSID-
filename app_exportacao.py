2147e67d461741a98040649233ea22cf6e5fd8e7
grosbelli12 <gustavogrosbelli11@gmail.com>
2026-07-06 10:57:55 -0300

    separado por pagina de conexão e exportação e add criptografia

--- app_exportacao.py
+++ app_exportacao.py
@@ -1,12 +1,39 @@
-from tkcalendar import Calendar
 import customtkinter as ctk
+import os
+from tkcalendar import Calendar
 from baixar_arquivos import processar_exportacao
+from cryptography.fernet import Fernet
 
 # Configuração da janela principal
 app = ctk.CTk()
 app.geometry("700x700")
 app.title("Exportador de Arquivos")
 
+# Cria uma chave mestra para criptografia se não existir
+if not os.path.exists("chave.key"):
+    chave_nova = Fernet.generate_key()
+    with open("chave.key", "wb") as arquivo_chave:
+        arquivo_chave.write(chave_nova)
+    
+    # Carregamos a chave para a memória do programa 
+    # fernet é o cofre que vai criptografar e descriptografar os dados
+with open("chave.key", "rb") as arquivo_chave:
+    chave_mestra = arquivo_chave.read()
+    fernet = Fernet(chave_mestra) 
+    
+def salvar_dados():
+    ip = input_ip.get()
+    nome_banco = input_nome_banco.get()
+    usuario = input_usuario.get()
+    senha_digitada = input_senha.get()
+    senha = fernet.encrypt(senha_digitada.encode()).decode()
+
+    with open("dados_conexao.txt", "w") as arquivo:
+        arquivo.write(f"{ip}\n{nome_banco}\n{usuario}\n{senha}")
+
+    label_aviso.configure(text="Dados salvos com sucesso!", text_color="green")
+    pagina_conexao.pack_forget()
+    pagina_exportacao.pack(fill="both", expand=True)
 
 def iniciar_exportacao():
     ip = input_ip.get()
@@ -44,7 +71,7 @@ def abrir_calendario_inicio():
     pos_x = btn_abrir.winfo_rootx()
     pos_y = btn_abrir.winfo_rooty()
 
-    janela_calendario.geometry(f"300x300+{pos_x}+{pos_y - 48 }")
+    janela_calendario.geometry(f"300x300+{pos_x}+{pos_y}")
 
     # Cria o calendário date_pattern já formata do jeito que o Postgres utiliza
     calendario = Calendar(janela_calendario, selectmode='day', date_pattern='yyyy-mm-dd')
@@ -67,8 +94,8 @@ def abrir_calendario_fim():
     janela_calendario.attributes("-topmost", True)
     janela_calendario.focus()
     
-    pos_x = btn_abrir.winfo_rootx()
-    pos_y = btn_abrir.winfo_rooty()
+    pos_x = btn_abrir_fim.winfo_rootx()
+    pos_y = btn_abrir_fim.winfo_rooty()
     
     janela_calendario.geometry(f"300x300+{pos_x}+{pos_y}")
 
@@ -88,12 +115,17 @@ def abrir_calendario_fim():
 
 
 
+
+
 titulo = ctk.CTkLabel(app, text="Exportador de Arquivos", font=("Arial", 20, "bold"))
 titulo.pack(pady=10)
 
 
 
-frame_ip = ctk.CTkFrame(app, fg_color="transparent")
+pagina_conexao = ctk.CTkFrame(app, fg_color="transparent")
+
+
+frame_ip = ctk.CTkFrame(pagina_conexao, fg_color="transparent")
 frame_ip.pack(pady=10)
 
 input_ip = ctk.CTkEntry(frame_ip, placeholder_text="localhost", font=("Arial", 14),  width=220)
@@ -104,7 +136,7 @@ ip.pack(side="right")
 
 
 
-frame_nome_banco = ctk.CTkFrame(app, fg_color="transparent")
+frame_nome_banco = ctk.CTkFrame(pagina_conexao, fg_color="transparent")
 frame_nome_banco.pack(pady=10)
 
 input_nome_banco = ctk.CTkEntry(frame_nome_banco, placeholder_text="Nome do Banco", font=("Arial", 14), width=220)
@@ -115,7 +147,8 @@ nome_banco.pack(side="right")
 
 
 
-frame_usuario = ctk.CTkFrame(app, fg_color="transparent")
+
+frame_usuario = ctk.CTkFrame(pagina_conexao, fg_color="transparent")
 frame_usuario.pack(pady=10)
 
 input_usuario = ctk.CTkEntry(frame_usuario, placeholder_text="Usuário",  font=("Arial", 14), width=220)
@@ -126,7 +159,7 @@ usuario.pack(side="right")
 
 
 
-frame_senha = ctk.CTkFrame(app, fg_color="transparent")
+frame_senha = ctk.CTkFrame(pagina_conexao, fg_color="transparent")
 frame_senha.pack(pady=10)
 
 input_senha = ctk.CTkEntry(frame_senha, placeholder_text="Senha", font=("Arial", 14), width=220)
@@ -136,8 +169,15 @@ senha = ctk.CTkLabel(frame_senha, text="Senha", font=("Arial", 14),width=130, an
 senha.pack(side="right")
 
 
+btn_salvar = ctk.CTkButton(pagina_conexao, text="Salvar Dados", command=salvar_dados,  width=220, hover_color="#3878D8", corner_radius=10)
+btn_salvar.pack(pady=10, padx=(140, 60))
 
-frame_cliente = ctk.CTkFrame(app, fg_color="transparent")
+
+
+pagina_exportacao = ctk.CTkFrame(app, fg_color="transparent")
+
+
+frame_cliente = ctk.CTkFrame(pagina_exportacao, fg_color="transparent")
 frame_cliente.pack(pady=(80,10))
 
 input_cliente = ctk.CTkEntry(frame_cliente, placeholder_text="0 para Todos", width=220)
@@ -148,7 +188,7 @@ cliente.pack(side="right")
 
 
 
-frame_data_inicio = ctk.CTkFrame(app, fg_color="transparent")
+frame_data_inicio = ctk.CTkFrame(pagina_exportacao, fg_color="transparent")
 frame_data_inicio.pack(pady=10)
 
 data_inicio = ctk.CTkLabel(frame_data_inicio, text="Data de Início", font=("Arial", 14), width=130, anchor="e")
@@ -163,7 +203,8 @@ btn_abrir.pack(side="left")
 
 
 
-frame_data_fim = ctk.CTkFrame(app, fg_color="transparent")
+
+frame_data_fim = ctk.CTkFrame(pagina_exportacao, fg_color="transparent")
 frame_data_fim.pack(pady=10)
 
 data_fim = ctk.CTkLabel(frame_data_fim, text="Data de Fim", font=("Arial", 14), width=130, anchor="e")
@@ -172,13 +213,13 @@ data_fim.pack(side="left", padx=(0, 10))
 input_data_fim = ctk.CTkEntry(frame_data_fim, placeholder_text="Data de Fim (YYYY-MM-DD)", width=220)
 input_data_fim.pack(side="left", padx=(0, 10))
 
-btn_abrir = ctk.CTkButton(frame_data_fim, text="📅", command=abrir_calendario_fim, width=50)
-btn_abrir.pack(side="left")
+btn_abrir_fim= ctk.CTkButton(frame_data_fim, text="📅", command=abrir_calendario_fim, width=50)
+btn_abrir_fim.pack(side="left")
 
 
 
 
-frame_modulo = ctk.CTkFrame(app, fg_color="transparent")
+frame_modulo = ctk.CTkFrame(pagina_exportacao, fg_color="transparent")
 frame_modulo.pack(pady=10)
 
 modulo = ctk.CTkLabel(frame_modulo, text="Selecione o Módulo", font=("Arial", 14), width=130, anchor="e")
@@ -196,16 +237,36 @@ opcoes_modulos = [
     "8 - Contas a Pagar",
     "9 - Contas a Receber",
     "10 - Cheque de Terceiros         "
-]
+    ]
 combo_modulo = ctk.CTkComboBox(frame_modulo, values=opcoes_modulos, width=220)
 combo_modulo.set("0 - Todos") 
 combo_modulo.pack(side="right", padx=(0, 60))
 
-button_exportar = ctk.CTkButton(app, text="Exportar Arquivos", command=iniciar_exportacao, width=220, hover_color="#3878D8", corner_radius=10)
+button_exportar = ctk.CTkButton(pagina_exportacao, text="Exportar Arquivos", command=iniciar_exportacao, width=220, hover_color="#3878D8", corner_radius=10)
 button_exportar.pack(pady=30, padx=(140, 60))
 
-label_aviso = ctk.CTkLabel(app, text="", font=("Arial", 14))
+label_aviso = ctk.CTkLabel(pagina_exportacao, text="", font=("Arial", 14))
 label_aviso.pack(pady=10)
+
+
+if os.path.exists("dados_conexao.txt"):
+    with open("dados_conexao.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
+        ip_salvo = linhas[0].strip()
+        nome_banco_salvo = linhas[1].strip()
+        usuario_salvo = linhas[2].strip()
+        senha_salva = linhas[3].strip()
+        
+        senha = fernet.decrypt(senha_salva.encode()).decode()
+        input_ip.insert(0, ip_salvo)
+        input_nome_banco.insert(0, nome_banco_salvo)
+        input_usuario.insert(0, usuario_salvo)
+        input_senha.insert(0, senha)
+        
+        pagina_exportacao.pack(fill="both", expand=True)
+        
+else:
+        pagina_conexao.pack(fill="both", expand=True)
 
 # Mantém o app rodando
 app.mainloop()
